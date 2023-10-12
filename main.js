c3667ccec747f1f3e3c25a5a5bc99528979b7f45
Robin Kelsick II <robinkelsick@gmail.com>
2023-10-11 22:38:17 -0400

    Bot works with 3 basic commands. Need to look into creating my own commands.

--- main.js
+++ main.js
@@ -1,47 +1,52 @@
 const fs = require('fs');
-const path = require('path')
+const path = require('path');
 
 // Discord Classes
-const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 //Grab token from config file
-const {token} = require('./config.json');
+const { token } = require('./config.json');
 
 //Create new client instance
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection;
+client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, 'commands');
-const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-for (const file of commandsFiles) {
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
 
-    // New item in the collection with the key as the command name and the value is the exported module
-    if ('data' in command && 'execute' in command){
-        client.commands.set(command.data.name, command)
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
     } else {
-        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+      console.log(
+        `[WARNING] The command ${filePath} is missing required "data" or "execute" property`
+      );
     }
+  }
 }
 
-client.on(Events.InteractionCreate, interaction => {
-    console.log(interaction)
-})
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith('.js'));
 
-client.on(Events.InteractionCreate, interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    console.log(interaction)
-})
-
-// run this when the client is ready
-// C is the event parameter
-client.once(Events.ClientReady, c => {
-    // lets us know it is connected
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-})
+for (const file of eventFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 //Login into discord with token
 client.login(token);
-
